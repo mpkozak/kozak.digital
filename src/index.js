1e52f77bd32694127616edf589ef95b46d1338fe
Kozak, Michael <Michael.Kozak@mastercard.com>
2021-03-23 22:45:18 -0400

    removed serviceworker, added array.flat() polyfill for old safari, upgraded react.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import 'array-flat-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
-// import * as serviceWorker from './serviceWorker.js';
 
 
 
@@ -20,13 +20,3 @@ const isMobile = (() => {
 
 
 ReactDOM.render(<App isMobile={isMobile}/>, document.getElementById('root'));
-
-
-
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-// serviceWorker.register();
